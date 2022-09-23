480aaec33caa71b7b683564496bc20c761056d5d
sinazay123 <sinazay123@gmail.com>
2022-09-23 16:12:43 -0500

    Displayed Search component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import "./App.css";
 import Meals from "./components/Meals";
 // import Favorites from './components/Favorites';
 // import Modal from './components/Modal';
-// import Search from './components/Search';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
       <main>
-        {/* <Search />
-        <Favorites /> */}
+        <Search />
+        {/*<Favorites /> */}
         <Meals />
         {/* <Modal /> */}
       </main>
