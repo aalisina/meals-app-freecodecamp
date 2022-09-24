bb91f3fdc7c1aeed66fba8ca908358ac74153ccd
sinazay123 <sinazay123@gmail.com>
2022-09-23 20:34:31 -0500

    Added handleRandomMeal function to prevent a bug

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -17,6 +17,13 @@ function Search() {
     }
   };
 
+  // To prevent a bug when the user searches by a search term, then clicks the surprise me
+  // button and then searches by the same search term, the state doesn't change.
+  const handleRandomMeal = () => {
+    setText("");
+    setSearchTerm("");
+    fetchRandomMeal();
+  };
   return (
     <header className="search-container">
       <form onSubmit={handleSubmit}>
@@ -33,7 +40,7 @@ function Search() {
         <button
           type="button"
           className="btn btn-hipster"
-          onClick={fetchRandomMeal}
+          onClick={handleRandomMeal}
         >
           Surprise me!
         </button>
