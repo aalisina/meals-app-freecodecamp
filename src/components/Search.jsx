67da07519532c896b4172bc99d9fa9d5f62031d6
sinazay123 <sinazay123@gmail.com>
2022-09-23 20:06:09 -0500

    Added handleChange and handleSubmit for the text input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import { useGlobalContext } from "../Context";
 
 function Search() {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <header className="search-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Type your favorite meal."
           className="form-input"
+          onChange={handleChange}
+          value={text}
         />
         <button type="submit" className="btn">
           Search
