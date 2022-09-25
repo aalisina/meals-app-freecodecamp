51dd2b79cf383dafb4c2c12eb1f3af848f6900f5
sinazay123 <sinazay123@gmail.com>
2022-09-25 17:29:11 -0500

    Set a condition to only show the modal if the showModal variable is truthy in the context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Meals from "./components/Meals";
 // import Favorites from './components/Favorites';
-// import Modal from './components/Modal';
-import Search from './components/Search';
+import Modal from "./components/Modal";
+import Search from "./components/Search";
+import { useGlobalContext } from "./Context";
 
 function App() {
+  const { showModal } = useGlobalContext();
+
   return (
     <div className="App">
       <main>
         <Search />
         {/*<Favorites /> */}
         <Meals />
-        {/* <Modal /> */}
+        {showModal && <Modal />}
       </main>
     </div>
   );
