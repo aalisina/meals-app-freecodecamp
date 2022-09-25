3ca0c6f64c9cca942cbf5ec57f1806571e6449f2
sinazay123 <sinazay123@gmail.com>
2022-09-25 17:22:23 -0500

    Added some classes to the Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useGlobalContext } from "../Context";
 
 function Modal() {
-  return <h1>Modal component. 4:10</h1>;
+  return (
+    <aside className="modal-overlay">
+      <div className="modal-container">
+        <h3>Modal Container</h3>
+      </div>
+    </aside>
+  );
 }
 
 export default Modal;
