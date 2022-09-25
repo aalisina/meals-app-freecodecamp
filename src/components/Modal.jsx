1d1f8e8f6582b89a87e58ac2b659bd067d818d2f
sinazay123 <sinazay123@gmail.com>
2022-09-25 18:21:35 -0500

    Added closeMeal and selectedMeal to the modal but the selectedMeal isn't going through

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useGlobalContext } from "../Context";
 
 function Modal() {
+  const { closeModal, selectedMeal } = useGlobalContext();
   return (
     <aside className="modal-overlay">
       <div className="modal-container">
-        <h3>Modal Container</h3>
+        <h3>{selectedMeal} Meal</h3>
+        <button onClick={closeModal}>Close</button>
       </div>
     </aside>
   );
