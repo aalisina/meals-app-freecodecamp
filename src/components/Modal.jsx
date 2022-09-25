7c4457e62fb708249b60c9514f8d8bbb0307dbcd
sinazay123 <sinazay123@gmail.com>
2022-09-25 18:52:51 -0500

    Added the html elements for the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,11 +3,25 @@ import { useGlobalContext } from "../Context";
 
 function Modal() {
   const { closeModal, selectedMeal } = useGlobalContext();
+  const {
+    strMealThumb: image,
+    strMeal: title,
+    strInstructions: text,
+    strSource: source,
+  } = selectedMeal;
   return (
     <aside className="modal-overlay">
       <div className="modal-container">
-        <h3>{selectedMeal.strMeal}</h3>
-        <button onClick={closeModal}>Close</button>
+        <img src={image} alt={title} className="img modal-img" />
+        <div className="modal-content">
+          <h4>{title}</h4>
+          <p>Cooking Instructions</p>
+          <p>{text}</p>
+          <a href={source} target="_blank">
+            Original Source
+          </a>
+          <button onClick={closeModal}>Close</button>
+        </div>
       </div>
     </aside>
   );
