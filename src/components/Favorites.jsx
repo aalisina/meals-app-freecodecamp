bd29989f6c0d1db6a9f15c534f0d88039d957463
sinazay123 <sinazay123@gmail.com>
2022-09-25 21:50:10 -0500

    Added the html elements for the favorites component

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useGlobalContext } from "../Context";
 
 function Favorites() {
-  return <h1>Favorite component!</h1>;
+  const { favorites, removeFromFavorites, selectMealFunc } = useGlobalContext();
+  return (
+    <section className="favorites">
+      <div className="favorites-content">
+        <h5>Favorites</h5>
+        <div className="favorites-container">
+          {favorites.map((favoriteMeal) => {
+            const {
+              idMeal,
+              strMealThumb: image,
+              strMeal: title,
+            } = favoriteMeal;
+            return (
+              <div key={idMeal} className="favorite-item">
+                <img src={image} alt={title} className="favorites-img img" />
+                <button
+                  className="remove-btn"
+                  onClick={() => removeFromFavorites(idMeal)}
+                >
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Favorites;
