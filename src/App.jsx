5df55b2797042ee6f73c50a35913f15a74dd698a
sinazay123 <sinazay123@gmail.com>
2022-09-25 21:35:01 -0500

    Render favorites component only when there are favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import Meals from "./components/Meals";
-// import Favorites from './components/Favorites';
+import Favorites from "./components/Favorites";
 import Modal from "./components/Modal";
 import Search from "./components/Search";
 import { useGlobalContext } from "./Context";
 
 function App() {
-  const { showModal } = useGlobalContext();
+  const { showModal, favorites } = useGlobalContext();
 
   return (
     <div className="App">
       <main>
         <Search />
-        {/*<Favorites /> */}
+        {favorites.length > 0 && <Favorites />}
         <Meals />
         {showModal && <Modal />}
       </main>
